165b8ff1e88664cab8c19ea61bf13d934733cca6
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-03-13 14:55:04 +0900

    [ Feat ] Animation 연결 작업

--- Assets/Scripts/05_Enemy/EnemyManager.cs
+++ Assets/Scripts/05_Enemy/EnemyManager.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemyPrefab;  // 스켈레톤 프리팹
+    public int enemyCount = 2;      // 스폰할 적 개수
+    public Vector3 spawnArea;       // 스폰 범위
+
     void Start()
     {
-        
+        SpawnEnemies();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnEnemies()
     {
-        
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Vector3 spawnPos = new Vector3(
+                Random.Range(-spawnArea.x, spawnArea.x),
+                0,
+                Random.Range(-spawnArea.z, spawnArea.z)
+            );
+
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+        }
     }
 }
+
