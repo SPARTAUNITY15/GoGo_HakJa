760b9fc910a55f56ea2bae063a7237526fb795e9
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-03-17 16:06:23 +0900

    ✨<Feat>: “구름, 오브젝트 배치, 오아시스 배치, 지형 평탄화”

--- Assets/Scripts/03_Map/PlacementGenerator.cs
+++ Assets/Scripts/03_Map/PlacementGenerator.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class PlacementGenerator : MonoBehaviour
 {
+    [SerializeField] Transform parent;
     [SerializeField] GameObject prefab;
 
     [Header("Raycast Setting")]
@@ -19,19 +21,28 @@ public class PlacementGenerator : MonoBehaviour
     [SerializeField] Vector2 zRange;
 
     [Header("Prefab Variation Setting")]
+    [SerializeField] bool isOasis = false;
     [SerializeField, Range(0, 1)] float rotateTowardsNormal;
     [SerializeField] Vector2 rotationRange;
     [SerializeField] Vector3 minScale;
     [SerializeField] Vector3 maxScale;
+    [SerializeField] float transformY = 0;
 
     private void Start()
     {
-        Generate();
+        if (!isOasis)
+        {
+            Generate();
+        }
+        else
+        {
+            GenerateOasis();
+        }
     }
 
     public void Generate()
     {
-        Clear();
+        int floorLayer = LayerMask.GetMask("Floor");
 
         for (int i = 0; i < destiny; i++)
         {
@@ -39,7 +50,7 @@ public class PlacementGenerator : MonoBehaviour
             float sampleY = Random.Range(zRange.x, zRange.y);
             Vector3 ray = new Vector3(sampleX, maxHeight, sampleY);
 
-            if (!Physics.Raycast(ray, Vector3.down, out RaycastHit hit, Mathf.Infinity))
+            if (!Physics.Raycast(ray, Vector3.down, out RaycastHit hit, Mathf.Infinity, floorLayer))
             {
                 continue;
             }
@@ -49,7 +60,8 @@ public class PlacementGenerator : MonoBehaviour
             }
 
             GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, transform);
-            go.transform.position = hit.point;
+            go.transform.parent = parent;
+            go.transform.position = hit.point - new Vector3(0, transformY, 0);
             go.transform.Rotate(Vector3.up, Random.Range(rotationRange.x, rotationRange.y), Space.Self);
             go.transform.rotation = Quaternion.Lerp(transform.rotation,
                                                     transform.rotation * Quaternion.FromToRotation(go.transform.up, hit.normal),
@@ -62,11 +74,35 @@ public class PlacementGenerator : MonoBehaviour
         }
     }
 
-    public void Clear()
+    void GenerateOasis()
     {
-        while (transform.childCount != 0)
+        int floorLayer = LayerMask.GetMask("Floor");
+        int i = 0;
+
+        while (i < destiny)
         {
-            DestroyImmediate(transform.GetChild(0).gameObject);
+            float sampleX = xRange.x;
+            float sampleY = zRange.x;
+            Vector3 ray = new Vector3(sampleX, maxHeight, sampleY);
+
+            if (!Physics.Raycast(ray, Vector3.down, out RaycastHit hit, Mathf.Infinity, floorLayer))
+            {
+                continue;
+            }
+
+            GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, transform);
+            go.transform.parent = parent;
+            go.transform.position = hit.point - new Vector3(0, transformY, 0);
+            go.transform.Rotate(Vector3.up, Random.Range(rotationRange.x, rotationRange.y), Space.Self);
+            go.transform.rotation = Quaternion.Lerp(transform.rotation,
+                                                    transform.rotation * Quaternion.FromToRotation(go.transform.up, hit.normal),
+                                                    rotateTowardsNormal);
+            go.transform.localScale = new Vector3(
+                Random.Range(minScale.x, maxScale.x),
+                Random.Range(minScale.y, maxScale.y),
+                Random.Range(minScale.z, maxScale.z)
+                );
+            i++;
         }
     }
 }
