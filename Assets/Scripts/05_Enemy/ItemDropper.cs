f8e83de96f3b75e072ba284ef8020bc81ab5b0cb
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-03-17 11:15:16 +0900

    <Fix>"영준님 수정사항"

--- Assets/Scripts/05_Enemy/ItemDropper.cs
+++ Assets/Scripts/05_Enemy/ItemDropper.cs
@@ -20,14 +20,13 @@ public class ItemDropper : MonoBehaviour
     }
     public void DropItem()
     {
-        if (dropItemPrefab != null)
+        if (itemData != null)
         {
-            // 현재 위치에서 아이템 생성
-            GameObject droppedItem = Instantiate(dropItemPrefab, transform.position, Quaternion.identity);
-            //GameObject go = itemData.ToDropItem(transform.position, Quaternion.identity);
+            //GameObject droppedItem = Instantiate(dropItemPrefab, transform.position, Quaternion.identity);
+            GameObject go = itemData.ToDropItem(transform.position, Quaternion.identity);
             
-            Rigidbody rb = droppedItem.GetComponent<Rigidbody>();
-            //Rigidbody rb = go.GetComponent<Rigidbody>();
+            //Rigidbody rb = droppedItem.GetComponent<Rigidbody>();
+            Rigidbody rb = go.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 Vector3 randomForce = new Vector3(Random.Range(-1f, 1f), 1f, Random.Range(-1f, 1f)) * dropForce;
