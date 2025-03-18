ef14a505bd93d009cacf238dd24933faa708d89d
EunJi <euuu0eeu@gmail.com>
2025-03-18 09:22:39 +0900

    <Feat>: "StartScene 추가"

---
+++ Assets/Scripts/01_ Manager/_SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _SceneManager : MonoBehaviour
+{
+    public void MainGame()
+    {
+        //SceneManager.LoadScene("MainScene");
+    }
+}
