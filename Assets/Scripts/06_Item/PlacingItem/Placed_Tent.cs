206b01cba98a0ceca5fca115455f79fd008e6de3
YJ402 <uky0916@gmail.com>
2025-03-19 11:07:39 +0900

    [feat] 준비된 세이프존 텐트에 병합

--- Assets/Scripts/06_Item/PlacingItem/Placed_Tent.cs
+++ Assets/Scripts/06_Item/PlacingItem/Placed_Tent.cs
@@ -6,7 +6,10 @@ public class Placed_Tent : Placed_Item
 {
     private void Start()
     {
-        //세이프존 생성
+        //세이프존 오브젝트 생성
+        GameObject safezone = new GameObject("SafeZone");
+        safezone.transform.SetParent(transform,false);
+        safezone.AddComponent<SafeZone>();
     }
 
     public override void SubscribeMethod()
